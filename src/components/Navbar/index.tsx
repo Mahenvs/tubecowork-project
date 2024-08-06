0746b426ee7bd62201136c37a74d991c7d1e3953
mahenv <mahendrauppari044@gmail.com>
2024-08-06 00:54:33 -0400

    Added Dropdown on Profile icon #20

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react"
+import React, { useState } from "react"
 
 import { useSession, signIn, signOut } from "next-auth/react"
 import Button from "@/components/Buttons/Button"
@@ -10,6 +10,7 @@ import Link from "next/link"
 function Navbar() {
     const { data: session } = useSession()
     const pathname = usePathname()
+    const [showLogOut, setShowLogOut] = useState<boolean>(false)
     return (
         <nav className="flex justify-between items-center px-12 py-4 shadow-xl h-[8vh]">
             <Link href="/">
@@ -44,8 +45,25 @@ function Navbar() {
                             width={50}
                             height={50}
                             alt="logo"
+                            onClick={() => setShowLogOut(!showLogOut)}
                             className="rounded-full cursor-pointer"
                         />
+                        {showLogOut && (
+                            <div className="absolute mt-28 right-4">
+                                <div className="relative inline-block">
+                                    <Button
+                                        text="Sign Out"
+                                        onClick={() =>
+                                            signOut({ callbackUrl: "/" })
+                                        }
+                                        className="px-4 py-2 bg-b lue-500 text-white rounded-md focus:outline-none"
+                                    ></Button>
+                                    <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-full mt-2">
+                                        <div className="w-3 h-3 bg-primary transform rotate-45"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                         {/* <Image src={"/images/avatar.png"} width={40} height={40} alt="logo" /> */}
                     </>
                 ) : (
